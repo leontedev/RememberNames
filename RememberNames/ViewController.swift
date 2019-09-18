7329ce25a454af94b6e25df02931cc544a406e79
Mihail87 <leontemihai@gmail.com>
2019-09-18 11:06:21 +0300

    Day 44: Challenge #1 completed

--- RememberNames/ViewController.swift
+++ RememberNames/ViewController.swift
@@ -73,16 +73,29 @@ class ViewController: UICollectionViewController, UIImagePickerControllerDelegat
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         let person = people[indexPath.item]
-        let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
-        ac.addTextField()
-        ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak ac] _ in
-            guard let newName = ac?.textFields?[0].text else { return }
-            person.name = newName
+        
+        
+        // Challenge #1: add an ac with Rename & Delete options
+        let ac = UIAlertController(title: "Select action", message: "Do you want to rename or delete this person?", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { [weak self, weak ac] _ in
+            self?.people.remove(at: indexPath.item)
             self?.collectionView.reloadData()
         }))
-        
-        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        ac.addAction(UIAlertAction(title: "Rename", style: .default, handler: { [weak self, weak ac] _ in
+            let ac = UIAlertController(title: "Rename person", message: nil, preferredStyle: .alert)
+            ac.addTextField()
+            ac.addAction(UIAlertAction(title: "OK", style: .default, handler: { [weak self, weak ac] _ in
+                guard let newName = ac?.textFields?[0].text else { return }
+                person.name = newName
+                self?.collectionView.reloadData()
+            }))
+            
+            ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            self?.present(ac, animated: true)
+        }))
         present(ac, animated: true)
+        
+        
     }
 
 }
